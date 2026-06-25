e72c21bfa494a7d01ec84dc91ff3f2d32cf246ec
unknown <stepa.yakovkin@gmail.com>
2026-06-25 11:16:04 +0200

    Add packages for EF core. Add models and relations in EShopDbContext

---
+++ EShopData/Data/EShopDbContext.cs
@@ -0,0 +1,35 @@
+﻿using EShopData.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopData.Data
+{
+    public class EShopDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserInfo> UserInfos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }    
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: connection
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>().HasKey(ui => ui.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.UserInfo)
+                .WithOne(ui => ui.User)
+                .HasForeignKey<UserInfo>(ui => ui.UserId);
+        }
+    }
+}
