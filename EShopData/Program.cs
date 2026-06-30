653da0d694f64c580bcb42574da6f7c37a1193c4
unknown <stepa.yakovkin@gmail.com>
2026-06-30 13:30:12 +0200

    Add host and AppSettting.json

--- EShopData/Program.cs
+++ EShopData/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace EShopData
+﻿using EShopData.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EShopData
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var hostBuilder = Host.CreateDefaultBuilder(args);
+
+            hostBuilder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<EShopDbContext>(options =>
+                    options.UseNpgsql(context.Configuration.GetConnectionString("DefaultConnection")));
+            });
+
+            var host = hostBuilder.Build();
     }
+}
 }
