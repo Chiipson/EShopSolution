b150c1610fafb4efc6ddde654271ebc61b7a34c0
unknown <stepa.yakovkin@gmail.com>
2026-07-06 17:40:04 +0200

    start realization of product and category services

---
+++ EShopData/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using EShopData.Data;
+using EShopData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopData.Services
+{
+    public class CategoryService
+    {
+        private readonly EShopDbContext _context;
+
+        public CategoryService(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public Category GetById(int id) => _context.Categories.Find(id);
+
+        public Category GetByName(string name) => _context.Categories.Single(c => c.Name == name);
+
+        public IEnumerable<Category> GetAll() => _context.Categories.ToList();
+    }
+}
---
+++ EShopData/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using EShopData.Data;
+using EShopData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopData.Services
+{
+    public class ProductService
+    {
+        private readonly EShopDbContext _context;
+
+        public ProductService(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
