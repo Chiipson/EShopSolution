f895ac3f49c351d870b99ba796ad16c292fc87b9
Stsiapan Yakaukin <stepa.yakovkin@gmail.com>
2026-07-23 12:37:22 +0200

    add MainMenu skeleton and add arrow key navigation

---
+++ EShopData/Common/ConsoleHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopData.Common
+{
+    public class ConsoleHelper
+    {
+        public int ShowArrowMenu(string title,string[] options)
+        {
+            int chosenRow = 0;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine(title + '\n');
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == chosenRow)
+                    {
+                        Console.Write("> ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(options[i]);
+                }
+
+                var key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        chosenRow--;
+                        if (chosenRow < 0)
+                        {
+                            chosenRow = options.Length - 1;
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        chosenRow++;
+                        if (chosenRow >= options.Length)
+                        {
+                            chosenRow = 0;
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        return chosenRow;
+                }
+            }
+        }
+    }
+}
---
+++ EShopData/Menus/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using EShopData.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopData.Menus
+{
+    public class MainMenu
+    {
+        private readonly ConsoleHelper consoleHelper;
+        private readonly GuestMenu guestMenu;
+
+        public MainMenu(ConsoleHelper consoleHelper, GuestMenu guestMenu)
+        {
+            this.consoleHelper = consoleHelper;
+            this.guestMenu = guestMenu;
+        }
+        public void Show()
+        {
+            var menuOptions = new string[]
+            {
+               "Login",
+               "Register",
+               "Continue as a Guest",
+               "Exit",
+            };
+            while (true)
+            {
+                var selected = consoleHelper.ShowArrowMenu("Eshop Appliccation", menuOptions);
+
+                switch (selected)
+                {
+                    case 0:
+                        //TODO: login menu
+                        break;
+                    case 1:
+                        //TODO: register menu
+                        break;
+                    case 2:
+                        //TODO: Guest menu
+                        break;
+                    case 3:
+                        Console.WriteLine("Exit");
+                        return;
+                }
+            }
+
+        }
+    }
+}
